1848ebef2f9cfaaded9ebabfaf01673e254ba873
David OG <ogunejimitedavid@gmail.com>
2023-04-16 22:17:18 +0100

    Request Logs Livewire Implementation

---
+++ app/Http/Livewire/RequestLogs.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\RequestLog;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class RequestLogs extends Component
+{
+
+    use WithPagination;
+    public $request_id;
+    public $startdate;
+    public $perPage;
+    public $requestlogs;
+
+    public $total;
+    public $currentPage;
+    public $transactionCount;
+    private $enddate;
+
+
+
+    protected $paginationTheme = 'bootstrap';
+
+
+    public function render()
+    {
+
+        $this->perPage = ($this->perPage > 7000 ? 100: $this->perPage) ?? 20;
+        $criteria = [
+            "start_date" => $this->startdate,
+            'end_date' => $this->enddate,
+            'request_id' => $this->request_id
+        ];
+        $collection = RequestLog::criteria($criteria)->latest()->paginate($this->perPage);
+        $this->requestlogs = $collection->items();
+        $this->transactionCount = $collection->total();
+
+        return view('livewire.request-logs',[
+            'transactionCount' => $this->transactionCount,
+            'requestlogs' => $this->requestlogs,
+            'requestlogsCollection' => $collection,
+            'enddate' => $this->enddate,
+            'startdate' => $this->startdate,
+        ])->extends('layouts.admin.admin_dashboardapp', ['title' => 'Request Logs']);
+
+    }
+
+}
