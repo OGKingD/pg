b768f8f7acd8d1b5379002d6d1d67a54941be76c
David OG <ogunejimitedavid@gmail.com>
2024-05-16 13:25:04 +0100

    Update UI elements

--- resources/views/partials/admin/admin_footer.blade.php
+++ resources/views/partials/admin/admin_footer.blade.php
@@ -1,55 +1,50 @@
 
-<footer class="footer pt-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 mb-4 mx-auto text-center">
+<footer class="footer">
+        <div class="row text-center">
+            <div class="col-lg-8  mx-auto text-center">
                 <a href="https://saanapay.ng/index.html#counters6-1s" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     Company
                 </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                <a href="javascript:" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     About Us
                 </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                <a href="javascript:" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     Team
                 </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                <a href="javascript:" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     Products
                 </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                <a href="javascript:" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     Blog
                 </a>
                 <a href="https://saanapay.ng/index.html#content4-x" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     Pricing
                 </a>
             </div>
-            <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+            <div class="mt-1 text-center">
+                <a href="javascript:" target="_blank" class="text-secondary me-xl-4 me-4">
                     <span class="text-lg fab fa-dribbble" aria-hidden="true"></span>
                 </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                <a href="javascript:" target="_blank" class="text-secondary me-xl-4 me-4">
                     <span class="text-lg fab fa-twitter" aria-hidden="true"></span>
                 </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                <a href="javascript:" target="_blank" class="text-secondary me-xl-4 me-4">
                     <span class="text-lg fab fa-instagram" aria-hidden="true"></span>
                 </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                <a href="javascript:" target="_blank" class="text-secondary me-xl-4 me-4">
                     <span class="text-lg fab fa-pinterest" aria-hidden="true"></span>
                 </a>
-                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                <a href="javascript:" target="_blank" class="text-secondary me-xl-4 me-4">
                     <span class="text-lg fab fa-github" aria-hidden="true"></span>
                 </a>
             </div>
+            <p class="mb-0 text-secondary">
+                Copyright &copy; <script>
+                    document.write("" + new Date().getFullYear())
+                </script> <b class="text-primary text-gradient">Saanapay</b> Limited.
+            </p>
         </div>
-        <div class="row">
-            <div class="col-8 mx-auto text-center mt-1">
-                <p class="mb-0 text-secondary">
-                    Copyright © <script>
-                        document.write(new Date().getFullYear())
-                    </script> <b class="text-primary text-gradient">Saanapay</b> Limited.
-                </p>
-            </div>
-        </div>
-    </div>
+
     <script src="{{asset('assets/js/soft-ui-dashboard.min.js')}}"></script>
     <script src="{{asset('assets/js/app.js')}}"></script>
 </footer>
